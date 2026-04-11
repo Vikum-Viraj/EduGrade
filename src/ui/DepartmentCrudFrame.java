ce9e3b706938a0e3cddb8685e835b912ebb11076
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2026-04-11 09:25:45 +0530

    refactor MVC

--- src/ui/DepartmentCrudFrame.java
+++ src/ui/DepartmentCrudFrame.java
@@ -16,7 +16,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
